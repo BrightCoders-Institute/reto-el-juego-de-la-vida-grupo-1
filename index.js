2361ef15b7f874d65c6cca7b8f293de77de9301f
ElizabethFlores <lizflores.dev@gmail.com>
2023-05-31 18:37:08 -0600

    Update initial grid to a random grid
    Co-authored-by:JaimeGonz<valdoc7@gmail.com>
    Co-authored-by:Raibrr<ramon.ibrr@gmail.com>

--- index.js
+++ index.js
@@ -10,15 +10,29 @@
 // [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
 // ];
 
-let grid = [ 
-	[0,0,0,0],
-	[0,0,1,1],
-	[0,1,1,0],
-	[0,0,0,0]
-]
 
-let rows = 4;
-let columns = 4;
+let rows = 10;
+let columns = 10;
+let grid=creategrid();
+setinitialgrid();
+
+function creategrid() {
+const grid=[];
+	for (let i = 0; i < rows; i++) {
+		grid[i] = new Array(columns);
+	}
+return grid;
+}
+
+function setinitialgrid() {
+
+	for (let i = 0; i < columns; i++) {
+		for (let j = 0; j < rows; j++) {
+			grid[i][j] = Math.round(Math.random(2));
+		}
+	}
+
+}
 
 function printGrid() {
 	console.log("Generation:");
@@ -27,64 +41,71 @@ function printGrid() {
 		for (let col = 0; col < columns; col++) {
 			rowString += grid[row][col] === 1 ? " X " : " - ";
 		}
-			console.log(rowString);
+		console.log(rowString);
 	}
 	console.log("-------------------");
 }
 
 function countLiveNeighbors(row, col) {
 	let count = 0;
-  
+
 	for (let i = -1; i <= 1; i++) {
-	  for (let j = -1; j <= 1; j++) {
+		for (let j = -1; j <= 1; j++) {
 
-		if (i === 0 && j === 0) continue;
-		
-		const neighborRow = row + i;
-		const neighborCol = col + j;
+			if (i === 0 && j === 0) continue;
 
-		if (
-		  neighborRow >= 0 &&
-		  neighborRow < rows &&
-		  neighborCol >= 0 &&
-		  neighborCol < columns &&
-		  grid[neighborRow][neighborCol] === 1
-		) {
-		  count++;
-		//   console.log(count);
+			const neighborRow = row + i;
+			const neighborCol = col + j;
+
+			if (
+				neighborRow >= 0 &&
+				neighborRow < rows &&
+				neighborCol >= 0 &&
+				neighborCol < columns &&
+				grid[neighborRow][neighborCol] === 1
+			) {
+				count++;
+				//   console.log(count);
+			}
 		}
-	  }
 	}
 	return count;
 }
 
+function simulate() {
+	// Print the initial grid
+	printGrid();
+
+	// Simulate each generation
+	for (let generation = 1; generation <= 5; generation++) {
+		updateGrid();
+		printGrid();
+	}
+}
+
 function updateGrid() {
 
-	const newGrid = new Array(rows);
-	for (let i = 0; i < rows; i++) {
-		newGrid[i] = new Array(columns);
-	}
-  
-	for (let row = 0; row < rows; row++) {
-	  for (let col = 0; col < columns; col++) {
-		const cell = grid[row][col];
-		const liveNeighbors = countLiveNeighbors(row, col);
-		// console.log(liveNeighbors);
+	const newGrid = creategrid()
 
-		if (cell === 1 && (liveNeighbors < 2 || liveNeighbors > 3)) {
-			newGrid[row][col] = 0; // Cell dies due to underpopulation or overpopulation
-		} else if (cell === 0 && liveNeighbors === 3) {
-			newGrid[row][col] = 1; // Cell becomes alive due to reproduction
-		} else {
-			newGrid[row][col] = cell; // Cell remains unchanged
+	for (let row = 0; row < rows; row++) {
+		for (let col = 0; col < columns; col++) {
+			const cell = grid[row][col];
+			const liveNeighbors = countLiveNeighbors(row, col);
+			// console.log(liveNeighbors);
+
+			if (cell === 1 && (liveNeighbors < 2 || liveNeighbors > 3)) {
+				newGrid[row][col] = 0; // Cell dies due to underpopulation or overpopulation
+			} else if (cell === 0 && liveNeighbors === 3) {
+				newGrid[row][col] = 1; // Cell becomes alive due to reproduction
+			} else {
+				newGrid[row][col] = cell; // Cell remains unchanged
+			}
 		}
-	  }
 	}
 
 	grid = newGrid;
 	// console.log(grid);
 }
 
-printGrid();
-updateGrid();
-printGrid();
+
+simulate()
